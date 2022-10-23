37cfab9bfadd69e50f0a4bae5e829a51ccf82c12
PramilaGhosh <pramila.ghosh@thoughtworks.com>
2022-10-23 11:55:53 +0530

    Refactor. another method call u autowire annotation

--- src/main/java/com/spring/annotationdemo/TennisCoach.java
+++ src/main/java/com/spring/annotationdemo/TennisCoach.java
@@ -3,17 +3,27 @@ package com.spring.annotationdemo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.swing.*;
+
 
 //if we not specify bean id then it automatically takes className as a bean id but first letter is small letter in the class in bean
 @Component
 public class TennisCoach implements Coach {
+    @Autowired
     private FortuneService fortuneService;
 
-    @Autowired
+
     public TennisCoach(FortuneService fortuneService) {
         this.fortuneService = fortuneService;
     }
 
+    //inject method
+    @Autowired
+    public void doSomeCrazyStuff(){
+        System.out.println("TennisCoach: inside doSomeCrazyStuff() method");
+    }
+
+    //this constructor is use for creating object by spring bean
     public TennisCoach() {
     }
 
